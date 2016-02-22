db03e35c2908a6ce9af0d5677fb4d21546f657c3
Annette Chun <amc4sq@virginia.edu>
2016-02-22 17:05:47 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,13 @@
+# Annette Chun
+# amc4sq
+# 02/22/16
+# hello.py
+
 __author__ = 'Annette Chun'
 
+def greeting(msg):
+  print(msg)
 if __name__ = "__main__":
-  print('hello')
+  msg = "hello"
+  greeting(msg)
 
